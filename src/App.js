5fb468024b230fdcbeb4dbb475fca2ee3f8a85b7
Basavarajbadiger <basavarajgb2207@gmail.com.com>
2024-03-03 20:59:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import XDictionary from "./Dictionary/Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <XDictionary />
+    </div>
+  );
+}
+
+export default App;
